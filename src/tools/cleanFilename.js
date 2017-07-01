7f5e80dc5902ad0a854646476918a9b75ded8d4e
Mialur <Mialur@users.noreply.github.com>
2017-07-02 01:44:48 +0300

    win32 pipe char

--- src/tools/cleanFilename.js
+++ src/tools/cleanFilename.js
@@ -1,10 +1,13 @@
 'use strict'
 
-const illegalRe = /[\/\?<:>\\\|\*\"]/g          // characters / ? < : > | * "
+const isWin32 = process.platform === 'win32'
+const illegalRe_X = /[\/\?<:>\\\*\"]/g            // characters / ? < : > * "
+const illegalRe_W = /[\/\?<:>\\\|\*\"]/g          // characters / ? < : > | * "
+const illegalRe = isWin32 ? illegalRe_W : illegalRe_X
 const controlRe = /[\x00-\x1f\x80-\x9f]/g       // control chars with code 0..31 and 128..159
 const reservedRe = /(^|\/)\.+/                  // remove starting . dots "/.." or ".."
 const windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i   // remove windows reserved names 
-const trailingPeriods = /\.+$/g                 // remove trailing periods
+const trailingPeriods = /[\.\s]+$/g                 // remove trailing periods and spaces
 
 /**
  * @param {string} original
